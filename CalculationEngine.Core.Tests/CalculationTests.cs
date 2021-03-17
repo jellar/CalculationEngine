9d5107bc210fa30dd7bf99151b2f3df083740063
Rajesh Jella <jella.rajesh@gmail.com>
2021-03-17 10:07:48 +0000

    Initial Commit

---
+++ CalculationEngine.Core.Tests/CalculationTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace CalculationEngine.Core.Tests
+{
+    public class CalculationTests
+    {
+        [Theory]
+        [InlineData(10, 20, 30)]
+        public void AdditionTest(int a, int b, int expected)
+        {
+            var result = Calculation.Add(a, b);
+            Assert.Equal(result, expected);
+        }
+
+    }
+}
